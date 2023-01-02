5fbf4cffd0478c86a6705ea18539de29a8887aed
toothemooon <35186738+toothemooon@users.noreply.github.com>
2023-01-02 22:55:10 +0800

    _

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
 
-function App() {
+const getEthereumObject = () => window.ethereum;
+
+const App = () => {
+  /*
+  * The passed callback function will be run when the page loads
+  * More technically, when the App component "Mounts".
+  */
+  useEffect(() => {
+    const ethereum = getEthereumObject();
+    if (!ethereum) {
+      console.log("Make sure you have metamask");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload~.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <div className="dataContainer">
+        👋 Hey there!
+      </div>
+
+      <div className="bio">
+        get to know how ethereum object injected into the browser window object~
+        connect the wallet and wave at me!
+      </div>
+
+      <button className="waveButton" onClick={null}>
+        Wave at me
+      </button>
     </div>
   );
-}
+};
 
 export default App;
