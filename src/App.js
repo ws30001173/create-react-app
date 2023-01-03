98779cd17322f6ca77e53a0b8281403339ed037a
toothemooon <35186738+toothemooon@users.noreply.github.com>
2023-01-03 23:52:04 +0800

    connectWallet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 const getEthereumObject = () => window.ethereum;
 
+/*
+ * This function returns the first linked account found.
+ * If there is no account linked, it will return null.
+ */
+const findMetaMaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    /*
+     * First make sure we have access to the Ethereum object.
+     */
+    if (!ethereum) {
+      console.error("Make sure you have Metamask installed!");
+      return null;
+    }
+
+    console.log("We have the Ethereum object", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts"});
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Find the connected account:", accounts);
+      return account;
+    } else {
+      console.log("No authorized account found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
 const App = () => {
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  // connect to wallet
+  const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+      });
+
+        console.log("Connected", accounts[0]);
+        setCurrentAccount(accounts[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
   /*
   * The passed callback function will be run when the page loads
   * More technically, when the App component "Mounts".
   */
   useEffect(() => {
-    const ethereum = getEthereumObject();
-    if (!ethereum) {
-      console.log("Make sure you have metamask");
-    } else {
-      console.log("We have the ethereum object", ethereum);
-    }
+    findMetaMaskAccount().then((account) => {
+      if (account !== null) {
+        setCurrentAccount(account);
+      }
+    });
   }, []);
 
   return (
     <div className="mainContainer">
       <div className="dataContainer">
+        <div className="header">
         👋 Hey there!
       </div>
 
@@ -31,8 +87,16 @@ const App = () => {
       <button className="waveButton" onClick={null}>
         Wave at me
       </button>
-    </div>
-  );
-};
 
+     {/* If there is no current account render this button */}
+  
+      {!currentAccount && (
+        <button className="waveButton" onClick={connectWallet}>
+         Connect Wallet
+        </button>
+      )}
+    </div>
+  </div>
+  );
+}
 export default App;
