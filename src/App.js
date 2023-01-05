5102eb0d8277e10edbc53a84916fdf1af2eecc31
toothemooon <35186738+toothemooon@users.noreply.github.com>
2023-01-05 22:15:20 +0800

    __

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json";
 
 const getEthereumObject = () => window.ethereum;
 
@@ -40,6 +42,10 @@ const App = () => {
 
   const [currentAccount, setCurrentAccount] = useState("");
 
+  const contractAddress = "0xa3a0d27CEf32B479e362A06AA003960cBA70e997";
+
+  // reference the abi content
+  const contractABI = abi.abi;
   // connect to wallet
   const connectWallet = async () => {
     try {
@@ -59,6 +65,26 @@ const App = () => {
         console.error(error);
       }
     };
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window; // equal to: const ethereum = window.ethereum;
+
+      if (ethereum) {
+        const provider = new ethers.providers.WebProvider(ethereum); // use nodes wallet provides to send and receive data
+        const signer = provider.getSigner();
+        // contract infos include ABI
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   /*
   * The passed callback function will be run when the page loads
@@ -84,7 +110,7 @@ const App = () => {
         connect the wallet and wave at me!
       </div>
 
-      <button className="waveButton" onClick={null}>
+      <button className="waveButton" onClick={wave}>
         Wave at me
       </button>
 
